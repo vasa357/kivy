07256202535272f752dcd8634e15b634faf8feef
Кравчук Василь <144225143+vasa357@users.noreply.github.com>
2024-03-28 18:05:46 +0200

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,12 +1,12 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
-from kivy.uix.boxlayout import Boxlayout
+from kivy.uix.boxlayout import BoxLayout
 class MyApp(App):
     def build(self):
         txt = Label(text = 'This is Text!!!')
         btn = Button(text = 'This is Button!!!')
-        layout = Boxlayout()
+        layout = BoxLayout()
         layout.add_widget(txt)
         layout.add_widget(btn)
         return layout
